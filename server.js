b868c9741ad27743c633bbb7bcb22695f5635c93
Jordan Larios <jolarios96@gmail.com>
2019-01-18 00:10:51 -0800

    scrape route able to scrape

--- server.js
+++ server.js
@@ -40,16 +40,39 @@ app.get("/", function (req, res) {
         msg: "Welcome",
     });
 });
-// 404 route (On unkown route)
-app.get("*", function (req, res) {
-    res.render("404");
-});
 
-// A GET route for scraping articles
 app.get("/scrape", function (req, res) {
+    request("#", function (error, response, body) {
+        var $ = cheerio.load(body);
 
-    // Send a message to the client
-    res.send("Scrape Complete");
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("article h2").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
+        });
+        console.log("Scrape compleete");
+        res.send("Scrape Complete");
+        // res.send(response.body);
+    });
 });
 
 // Route for getting all Articles from the db
@@ -66,3 +89,4 @@ app.get("/articles/:id", function (req, res) {
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 });
+// =======================================================================
